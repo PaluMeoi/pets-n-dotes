73205f48ecd3c7aea9308afa627d0d94440bdbe5
Fern <git@fern.one>
2024-04-02 09:11:12 -0700

    Remade tables with shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-
 import icon from "astro-icon";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [icon()]
+  integrations: [icon(), tailwind(), react()]
 });
